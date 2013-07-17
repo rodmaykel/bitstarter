d84af6d874b17c29812757f76b1dd0b2c96b8d30
Rod Michael Coronel <rodmaykel@gmail.com>
2013-07-18 03:45:29 +0800

    Add some description, etc

--- web.js
+++ web.js
@@ -7,6 +7,20 @@ app.get('/', function(request, response) {
   response.send(fs.readFileSync('index.html').toString());
 });
 
+app.get('/login', function(request, response) {
+  response.send(fs.readFileSync('login.html').toString());
+});
+
+app.get('/home', function(request, response) {
+  response.send(fs.readFileSync('home.html').toString());
+});
+
+app.get('/dashboard', function(request, response) {
+  response.send(fs.readFileSync('dashboard.html').toString());
+});
+
+
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
